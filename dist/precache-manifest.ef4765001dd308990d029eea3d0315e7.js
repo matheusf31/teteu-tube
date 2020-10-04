a531e92af12a4340d4c3d8e8bee6b84278bb057c
Matheus Filipe <mfcr031@gmail.com>
2020-10-04 14:36:17 -0300

    update

---
+++ dist/precache-manifest.ef4765001dd308990d029eea3d0315e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4c30777f6f73946f76a86cce2f16f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72f44856796aa7092b91",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "c8d4e7e31931d645a425",
+    "url": "/static/js/2.3fe61f2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3fe61f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f44856796aa7092b91",
+    "url": "/static/js/main.ec7cc2bb.chunk.js"
+  },
+  {
+    "revision": "713285e2f68ce04da427",
+    "url": "/static/js/runtime-main.aa0f7a79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
